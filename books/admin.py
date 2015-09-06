786274d236e81498cf60e162e1e2c7e9de6c546b
channeng <shannon@action.io>
2015-09-06 15:17:24 -0700

    Added custom admin views to models in Books app

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from books.models import Publisher, Author, Book
 
+# Classes to modify default views on admin page
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    # Adds a search bar on the top that searches the fields for admin user search terms
+    search_fields = ('first_name', 'last_name')
+    # Adds a filter bar on the right that lists unique values for 'last_name'
+    # Rmb add ',' comma at the end for single-field to indicate a tuple
+    list_filter = ('last_name',)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'publication_date')
+    list_filter = ('publication_date','authors')
+    # Provides a high-level drill-down bar at the top
+    date_hierarchy = 'publication_date'
+    # Provides default descending sort on publication date
+    ordering = ('-publication_date',)
+
+# Admin page automatically includes user groups and setings
+# Following code registers the app's model tables within the admin page
+# If a particular model table is not included, then it will not appear on the admin page
 admin.site.register(Publisher)
-admin.site.register(Author)
-admin.site.register(Book)
+# A modified admin view based on a admin model class definition above
+# It can be read as "Register the Author model with the AuthorAdmin options."
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(Book,BookAdmin)
--- books/models.py
+++ books/models.py
@@ -18,14 +18,27 @@ class Publisher(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    email = models.EmailField()
+    # blank = True makes the field accept blank values. 
+    # In this case, it makes email an optional field for Authors.
+    email = models.EmailField(blank=True)
     def __unicode__(self):
-        return self.first_name
+        return u'%s %s' % (self.first_name, self.last_name)
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
-    publication_date = models.DateField()
+    # By default, database fields are specified NOT null on creation
+    # While text accepts '' blank values, dates do not.
+    # In that case, we have to change date fields to accept null values
+    #    publication_date = models.DateField(blank=True, null=True)
+    # Before blank values can be accepted.
+    # When changing structure of database, use 'python manage.py dbshell and run the appropriate SQL command
+    # For Postgres SQL: 'ALTER TABLE books_book ALTER COLUMN publication_date DROP NOT NULL;'
+    # Not possible for SQLITE without deleting and recreating db table
+    publication_date = models.DateField(verbose_name='date published')
+    # From above, you can also pass verbose_name as a positional argument
+    #    publication_date = models.DateField('date published', other_fields='xxx')
+
     def __unicode__(self):
         return self.title
